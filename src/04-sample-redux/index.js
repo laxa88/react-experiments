c5b5253480bd2e2fc770bf600ddf70a5422fb263
Leong Wai Yin <gashzack@hotmail.com>
2017-06-21 14:18:33 +0800

    add Redux/Reducer initial code

---
+++ src/04-sample-redux/index.js
@@ -0,0 +1,7 @@
+import SampleComponent from './04-component';
+import SampleReducer from './04-reducer';
+
+export {
+  SampleComponent,
+  SampleReducer
+}
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SampleComponent from './01-sample-component';
 import {StatelessComponent,StatelessFunctionalComponent} from './02-stateless-component';
 import SampleButton from './03-sample-button';
+import * as SampleRedux from './04-sample-redux';
 
 export default class App extends React.Component {
   render() {
@@ -16,6 +17,10 @@ export default class App extends React.Component {
         <StatelessFunctionalComponent myClass="foo" myName="bar" />
 
         <SampleButton className="bar" onClick={()=>{alert("foo!")}}/>
+
+        <SampleRedux.SampleComponent
+
+        />
 
       </div>
     );
